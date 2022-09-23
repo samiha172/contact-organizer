fcb95f71ffde7ff2b499311da88a5dbafe775bc3
samiha172 <samiha.akhter@northsouth.edu>
2022-09-23 21:18:05 +0600

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,31 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    
+  },
+  {
+    path: '/contacts',
+    name: 'contacts',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contacts" */ '../views/ContactListView.vue')
+  },
+  {
+    path: '/create',
+    name: 'create',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "create" */ '../views/CreateNewContactView.vue')
+  },
+  {
+    path: '/contacts/edit/:contactId',
+    name: 'EditContact',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "EditContact" */ '../views/EditContactView.vue')
   }
 ]
 
